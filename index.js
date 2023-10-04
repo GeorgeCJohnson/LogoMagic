d1a25ea349af672869819e9c7c1188786c393a39
George J <Gjohnson.pioneer@gmail.com>
2023-10-03 23:58:10 -0400

    Add corrections to syntax in Circle shape in shapes.js.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const files = require
+const filesystem = require(`fs`)
+const inquirer = require(`inquirer`)
+const {Circle, Square, Triangle} = require(`./lib/shapes.js`);
+
+class SVG {
+    constructor() {
+        this.shapes = [];
+    }
+    add(shape) {
+        this.shapes.push(shape);
+    }
+    render() {
+        let svg = `<svg height="300" width="200">`;
+        this.shapes.forEach(shape => {
+            svg += shape.render();
+        });
+        svg += `</svg>`;
+        return svg;
+    }
+}
